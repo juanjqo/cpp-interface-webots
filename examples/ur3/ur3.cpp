373c56f074e0e71fb0fe83a67f738de1259cb9bf
Juancho <juanjqogm@gmail.com>
2025-07-25 15:29:58 +0100

    updated the minimal example for tests

--- examples/ur3/ur3.cpp
+++ examples/ur3/ur3.cpp
@@ -4,10 +4,20 @@
 #include <dqrobotics/interfaces/webots/DQ_WebotsInterface.h>
 #include <dqrobotics/interfaces/webots/robots/URXWebotsRobot.h>
 
+#include <signal.h>
 
+void handle_sigint(int sig){
+    std::cout<<"SIGINT received, but I don't care parcero"<<std::endl;
+}
 
 int main() {
+    //std::signal(SIGINT, handle_sigint);
+    if(signal(SIGINT, handle_sigint) == SIG_ERR)
+    {
+        throw std::runtime_error("::Error setting the signal int handler.");
+    }
     auto wb = std::make_shared<DQ_WebotsInterface>();
+
     wb->connect("ur3");
     wb->set_stepping_mode(true);
     wb->trigger_next_simulation_step();
@@ -16,14 +26,23 @@ int main() {
 
     VectorXd target = (VectorXd(6)<<pi/2,0,0,0,0,0).finished();
 
-    for(int i=0;i<100;i++)
+
+    for (int i=0;i<300;i++)
     {
-        URX->set_target_configuration(target);
-        auto q = URX->get_configuration();
-        DQ x = wb->get_object_pose(wb->get_robot_name());
-        std::cout<<x<<std::endl;
-        wb->trigger_next_simulation_step();
+        try {
+            URX->set_target_configuration(target);
+            auto q = URX->get_configuration();
+            DQ x = wb->get_object_pose(wb->get_robot_name());
+            std::cout<<i<<std::endl;
+            wb->trigger_next_simulation_step();
+            raise(SIGINT);
+            i++;
+        } catch (...) {
+            std::cout<<"Pailander"<<std::endl;
+        }
+
 
     }
+    std::cout<<"finishing...!"<<std::endl;
     wb->reset_simulation();
 }
