72e48987f9dc509d4446657c53ddb3f2a53ea0ef
Juancho <juanjqogm@gmail.com>
2025-07-28 13:30:49 +0100

    updated the example

--- examples/ur3/ur3.cpp
+++ examples/ur3/ur3.cpp
@@ -4,19 +4,23 @@
 #include <dqrobotics/interfaces/webots/DQ_WebotsInterface.h>
 #include <dqrobotics/interfaces/webots/robots/URXWebotsRobot.h>
 
-#include <signal.h>
-
-void handle_sigint(int sig){
-    std::cout<<"SIGINT received"<<std::endl;
+/*********************************************
+ * SIGNAL HANDLER
+ * *******************************************/
+#include<signal.h>
+static std::atomic_bool kill_this_process{false};
+void sig_int_handler(int)
+{
+    kill_this_process = true;
 }
 
 int main() {
-    //std::signal(SIGINT, handle_sigint);
+    // Since Webots captures the SIGINT signals, I initialize Webots first in order to
+    // override the SIGINT interruption.
     auto wb = std::make_shared<DQ_WebotsInterface>();
-    if(signal(SIGINT, handle_sigint) == SIG_ERR)
-    {
+    if(signal(SIGINT, sig_int_handler) == SIG_ERR)
         throw std::runtime_error("::Error setting the signal int handler.");
-    }
+
 
 
     wb->connect("ur3");
@@ -25,25 +29,23 @@ int main() {
 
     auto URX = std::make_shared<URXWebotsRobot>(wb, URXWebotsRobot::MODEL::UR3);
 
-    VectorXd target = (VectorXd(6)<<pi/2,0,0,0,0,0).finished();
-
-
-    for (int i=0;i<300;i++)
+    VectorXd target = (VectorXd(6)<<0,0,0,0,0,0).finished();
+    double T = 0.01;
+    unsigned int i = 0;
+    while(!kill_this_process)
     {
         try {
+            target(0) = sin(2*pi*i*T);
             URX->set_target_configuration(target);
             auto q = URX->get_configuration();
             DQ x = wb->get_object_pose(wb->get_robot_name());
-            std::cout<<i<<std::endl;
+            URX->set_target_configuration(target);
             wb->trigger_next_simulation_step();
-            raise(SIGINT);
             i++;
-        } catch (...) {
-            std::cout<<"catch"<<std::endl;
+        } catch (const std::runtime_error& e) {
+            std::cout<<e.what()<<std::endl;
         }
-
-
     }
-    std::cout<<"finishing...!"<<std::endl;
+    std::cout<<"Example finished!"<<std::endl;
     wb->reset_simulation();
 }
